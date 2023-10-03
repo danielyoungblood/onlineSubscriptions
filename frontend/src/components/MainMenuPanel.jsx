7f71ef25db46edf9aa492d6cac492ee3061752bb
daniel <danielyknechtli@gmail.com>
2023-10-02 17:06:36 -0700

    working on front end

--- frontend/src/components/MainMenuPanel.jsx
+++ frontend/src/components/MainMenuPanel.jsx
@@ -8,19 +8,38 @@ import {
   MDBCardText,
   MDBBtn,
 } from 'mdb-react-ui-kit';
+import SubscriptionListings from './SubscriptionListings';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+
 
 export default function MainMenuPanel() {
+  const [open, setOpen] = React.useState(false)
+  function handleClickOpen(){
+    setOpen(true);
+  };
+  function handleClose(){
+    setOpen(false);
+  }
   return (
     <MDBRow>
       <MDBCol sm='8' className='mb-3 mb-md-0'>
         <MDBCard>
           <MDBCardBody text='dark'>
-            <MDBBtn className='m-1' style={{ padding: '20px', width: '230px'}}> 
+            <MDBBtn className='m-1' style={{ padding: '20px', width: '230px'}} onClick={handleClickOpen}> 
           list of subscriptions
             </MDBBtn>
             <MDBBtn className='m-1' style={{ padding: '20px', width: '230px'}}>
               subscription management
             </MDBBtn>
+            <SubscriptionListings
+        open={open}
+        onClose={handleClose}
+      />
                   </MDBCardBody>
         </MDBCard>
       </MDBCol>
---
+++ frontend/src/components/SubscriptionListings.jsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import { MDBBtn} from 'mdb-react-ui-kit';
+import TextField from '@mui/material/TextField';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const response = await fetch('http://127.0.0.1:81/subscriptions');
+var rows = await response.json();
+
+export default function SubscriptionListings(props) {
+  const {open, onClose} = props
+
+    function handleClose() {
+      onClose()
+  };
+
+  return (
+    <div>  
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"subscription listings"}
+        </DialogTitle>
+        <DialogContent>
+          <TableContainer component={Paper}>
+      <Table width="100" aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name </TableCell>
+            <TableCell align="left">frequency</TableCell>
+            <TableCell align="center">cost</TableCell>
+          </TableRow>
+        </TableHead >
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.id}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="left">{row.frequency}</TableCell>
+              <TableCell align="center">{row.cost}</TableCell>
+              </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+          </DialogContent>
+        <DialogActions>
+            <Button onClick={handleClose}>
+            close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
